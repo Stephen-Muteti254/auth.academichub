f71623b142bec7206e6bb0b2abb371ab74f9f0f8
Stephen Muteti <stephenmuteti09@gmail.com>
2026-07-28 14:51:41 +0300

    Initial commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || 'https://api.academichubpro.com/api/v1';
-  // import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000/api/v1';
+  import.meta.env.VITE_API_URL ||
+  "https://api.academichubpro.com/api/v1";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
@@ -10,8 +10,9 @@ export const api = axios.create({
     'Content-Type': 'application/json',
   },
   withCredentials: true,
+  xsrfCookieName: "csrf_access_token",
+  xsrfHeaderName: "X-CSRF-TOKEN",
 });
-
 
 api.interceptors.request.use(
   (config) => {
@@ -20,10 +21,17 @@ api.interceptors.request.use(
       delete config.headers["Content-Type"];
     }
 
+    const match = document.cookie.match(
+        /(?:^|;\s*)csrf_access_token=([^;]+)/
+    );
+
+    if (match) {
+        config.headers["X-CSRF-TOKEN"] = decodeURIComponent(match[1]);
+    }
+
     return config;
   },
   (error) => Promise.reject(error)
 );
-
 
 export default api;
